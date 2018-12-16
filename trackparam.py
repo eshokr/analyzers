be2bc83053645d1a9035c1ba2cf816d2a5bda98f
eshokr <43872520+eshokr@users.noreply.github.com>
2018-12-16 03:15:42 +0200

    Create trackparam.py

---
+++ trackparam.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.categories.append('Demo')
+process.MessageLogger.cerr.INFO = cms.untracked.PSet(
+        limit = cms.untracked.int32(-1)
+        )
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200) )
+
+process.source = cms.Source("PoolSource",
+                                # replace 'myfile.root' with the source file you want to use
+                                fileNames = cms.untracked.vstring(
+
+            'root://eoscms.cern.ch//eos/cms/store/cmst3/group/das2014/tracking/tracks_and_vertices.root'
+                )
+                            )
+
+process.demo = cms.EDAnalyzer('trackparam',
+
+                                         minTracks=cms.untracked.uint32(0)
+                              )
+
+
+
+process.p = cms.Path(process.demo)
