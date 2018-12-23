2a24ce7840c7bf9e67928a4200e1d1cd41280c58
eshokr <43872520+eshokr@users.noreply.github.com>
2018-12-23 12:36:18 +0200

    Update eta().cc

--- eta().cc
+++ eta().cc
@@ -20,9 +20,6 @@
 #include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
-#include "TH1.h"
 
 //
 // class declaration
