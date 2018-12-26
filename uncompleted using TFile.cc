5e0dd2044c6c4d55323eab08989a47f7e980fe28
eshokr <43872520+eshokr@users.noreply.github.com>
2018-12-26 14:10:54 +0200

    Create uncompleted using TFile.cc

---
+++ uncompleted using TFile.cc
@@ -0,0 +1,172 @@
+#include <memory>
+
+// user include files
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TH1.h"
+#include "TFile.h"
+
+//
+// class declaration
+//
+
+class ElectronAnalyzer : public edm::EDAnalyzer {
+   public:
+      explicit ElectronAnalyzer(const edm::ParameterSet&);
+      ~ElectronAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+      //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+      //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+      //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+TH1D * histo;
+
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+ElectronAnalyzer::ElectronAnalyzer(const edm::ParameterSet& iConfig) 
+{
+   //now do what ever initialization is needed
+
+}
+
+
+ElectronAnalyzer::~ElectronAnalyzer()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+ElectronAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   
+   using namespace edm;
+using namespace std;
+
+
+Handle<vector<reco::GsfElectron>> gsfElectrons;
+  iEvent.getByLabel( "gsfElectrons",gsfElectrons);
+  
+   
+   
+ histo ->Fill(gsfElectrons.product()->size());
+ 
+
+
+   
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+   
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+ElectronAnalyzer::beginJob()
+{
+   TFile f("FirstRootFile.root");
+   histo = new TH1D("histo","Histo",100,0,40);
+   
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+ElectronAnalyzer::endJob() 
+{
+
+TFile f("FirstRootFile.root","recreate");
+histo ->Write();
+   f.Close();
+
+}
+
+// ------------ method called when starting to processes a run  ------------
+/*
+void 
+ElectronAnalyzer::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a run  ------------
+/*
+void 
+ElectronAnalyzer::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when starting to processes a luminosity block  ------------
+/*
+void 
+ElectronAnalyzer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+/*
+void 
+ElectronAnalyzer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+ElectronAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ElectronAnalyzer);
