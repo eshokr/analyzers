6407fb32dd02e38621f56db13b5effd6514fd9bf
eshokr <43872520+eshokr@users.noreply.github.com>
2019-03-07 13:06:22 +0200

    Update LAST.cc

--- LAST.cc
+++ LAST.cc
@@ -26,10 +26,10 @@
 // class declaration
 //
 
-class DemoAnalyzer5 : public edm::EDAnalyzer {
+class DemoAnalyzer6 : public edm::EDAnalyzer {
    public:
-      explicit DemoAnalyzer5(const edm::ParameterSet&);
-      ~DemoAnalyzer5();
+      explicit DemoAnalyzer6(const edm::ParameterSet&);
+      ~DemoAnalyzer6();
 
       static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
@@ -57,9 +57,15 @@ class DemoAnalyzer5 : public edm::EDAnalyzer {
   TH1D* histo1;
   TH1D* histo2;
   TH1D* histo5;
-   
+
+  TH1D* histod0;
+  TH1D* histod0pv; 
+  TH1D* histodz;
+  TH1D* histodzpv; 
+ 
   TH1D* histoNo;
-    TH1D* histoSl;
+    TH1D* histoSl1;
+TH1D* histoSl2;
   //TGraph* g;
   //TFile* f;
 
@@ -86,7 +92,7 @@ double dxxy=0.0; double dzz=0.0;
 //
 // constructors and destructor
 //
-DemoAnalyzer5::DemoAnalyzer5(const edm::ParameterSet& iConfig)
+DemoAnalyzer6::DemoAnalyzer6(const edm::ParameterSet& iConfig)
 {
 	tracksToken_ = mayConsume<std::vector<reco::Track> >(iConfig.getParameter<edm::InputTag> ("trck"));
 	vertexToken_ = mayConsume<std::vector<reco::Vertex>>(iConfig.getParameter<edm::InputTag>("vrtx"));
@@ -94,7 +100,7 @@ DemoAnalyzer5::DemoAnalyzer5(const edm::ParameterSet& iConfig)
 }
 
 
-DemoAnalyzer5::~DemoAnalyzer5()
+DemoAnalyzer6::~DemoAnalyzer6()
 {
  
    // do anything here that needs to be done at desctruction time
@@ -109,12 +115,13 @@ DemoAnalyzer5::~DemoAnalyzer5()
 
 // ------------ method called for each event  ------------
 void
-DemoAnalyzer5::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+DemoAnalyzer6::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 i = i+1;
 
    using namespace edm;
    using namespace std;
+//*******************************************************************************************************************************//
 cout <<"************************** begining of the event no( " << i << " )*****************************" << endl;
 //cout << "the number of this event is "<<i << endl;
 
@@ -143,6 +150,8 @@ vector <const reco::Vertex*> vertexvector;
 
 const reco::Vertex *vx = &(*primary_vertices ->begin());
 
+//******************************************************************************************************************************//
+
 //for loop to arrange the vertices again
  for (vector<reco::Vertex>::const_iterator vertices1 = primary_vertices->begin() ; vertices1 != primary_vertices->end(); vertices1 ++ )
 {
@@ -155,17 +164,15 @@ if (vertices1 ->tracksSize()== vx ->tracksSize())
 vertexvector.push_back(&*vertices1);
 }}
 
+//***********************************************************************************************************************************//
+// vxx is the vertex with the maxium track density
 const reco::Vertex *vxx = *vertexvector.begin();
 
-// to print vertexvector component 
+//**********************************************************************************************************************************//
+// to print the number of vertices in vertexvector
 cout <<"the number of vertices after selection " << vertexvector.size()<<endl;
 
-
-
-
- 
-
-
+//***********************************************************************************************************************************//
 
 dxxy = vx->position().Rho();
 dzz = vx ->position().Z();
@@ -174,18 +181,20 @@ cout << "the 1st primary vertex has "<<vx ->tracksSize() << " number of tracks" 
 cout <<"position of the 1st primary vertex in z "<<vx ->position().Z()<<" && in rho "<<vx ->position().Rho() <<endl; 
 cout <<"***********************************************************************"<<endl;
 
+//************************************************************************************************************************************//
 
   // print the number of primary vertex in this event to screen
 cout <<"----------------------------------------------------------------------------------" <<endl;
 cout << " ^_^ ^_^ the number of primary vertices in this event is = " << primary_vertices -> size () << endl;
    
+//*************************************************************************************************************************************//
+
    // to print the number of tracks in the primary vertices. also print the vertex position in z and rho
    for (vector<reco::Vertex>::const_iterator vertices = primary_vertices->begin() ; vertices != primary_vertices->end(); vertices ++ )
    {
  if(!vertices->isFake()) {
 u = u+1;
-cout <<"----------------------------------------------------------------------------------" <<endl; 
-cout <<"the number of this vertex is "<< u <<endl;
+cout <<"*************************** "<<"the number of this vertex is "<< u <<" ********************************"<<endl;
    cout << "  => Vertex has nTracks: " << vertices ->nTracks() << " and tracksSize:" << vertices ->tracksSize() << endl;
    cout << "  => position of vertex in z = " << vertices ->position().z() << " && in rho = " << vertices -> position().Rho() << endl;
 cout <<"----------------------------------------------------------------------------------" <<endl; 
@@ -195,45 +204,56 @@ if (vertices ->tracksSize() >= vx ->tracksSize())
 {
 dxxy = vertices ->position().Rho();
 dzz = vertices ->position().Z();
-
-
 }
- 
-//cout <<"-------------------------------------------------------------------" <<endl;  
+}
 }
 
-}
+//**********************************************************************************************************************************//
+
 cout <<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
  cout << "    ===> position of the vertex with maximum no of tracks in z = " << dzz << " && in rho = " << dxxy << endl;
 cout <<"      ===> position of the vertex with maxium no of tracks after selction in Z = " <<vxx->position().Z() <<" && in rho= "<<vxx->position().Rho() <<endl; 
 cout <<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
 
+//***********************************************************************************************************************************//
+
 for (reco::TrackCollection::const_iterator track2 = tracks->begin() ; track2 != tracks->end(); track2 ++ )
    {
 
 if (track2 -> quality(reco::Track::qualityByName("highPurity"))                  // 1- high quality     
-&& abs(track2 -> eta()) < 2.4                                              // 2- |η| < 2.4
-     && track2 ->pt() >0.1 && track2 ->pt() <5.0                                  // 3- pt  0.1 GeV/c < pt <5.0 GeV/c.
-     && track2 ->ptError()/track2 ->pt() < 0.1                                    // 4- σ(pT)/pT < 10 %
-     && abs(track2 ->dz()/ abs(track2 ->dzError()) < 3.0                              // 5- dz/ σ(dz) < 3
-     && abs(track2 ->dxy())/abs(track2 ->dxyError()) < 3.0    )) 
-j = j+1;
+     && fabs(track2 -> eta()) < 2.4                                              // 2- |η| < 2.4
+     && track2 ->pt() >0.4  && track2 ->pt() <5.0                                                     //3- pt >0.4 GeV/c.
+     && track2 ->ptError()/track2 ->pt() < 0.1                                  // 4- σ(pT)/pT < 10 %                                
+     && fabs(track2 ->dz()/ track2 ->dzError()) < 3.0                              // 5- dz/ σ(dz) < 3
+     && fabs(track2 ->dxy()/track2 ->dxyError()) < 3.0    ) 
 
+
+j = j+1;
 }
+
+//**********************************************************************************************************************************//
 
 for (reco::TrackCollection::const_iterator track3 = tracks->begin() ; track3 != tracks->end(); track3 ++ )
    {
 
 if (track3 -> quality(reco::Track::qualityByName("highPurity"))                  // 1- high quality     
-&& abs(track3 -> eta()) < 2.4                                              // 2- |η| < 2.4
-     && track3 ->pt() >0.1 && track3 ->pt() <5.0                                  // 3- pt  0.1 GeV/c < pt <5.0 GeV/c.
-     && track3 ->ptError()/track3 ->pt() < 0.1                                    // 4- σ(pT)/pT < 10 %
-     && abs(track3 ->dz(vxx ->position()))/ abs(track3 ->dzError()) < 3.0                              // 5- dz/ σ(dz) < 3
-     && abs(track3 ->dxy(vxx ->position()))/abs(track3 ->dxyError()) < 3.0    ) 
+     && fabs(track3 -> eta()) < 2.4                                              // 2- |η| < 2.4
+     &&track3 ->pt() >0.4   &&track3 ->pt() <5.0                                                     // 3- pt >0.4 GeV/c.
+     && track3 ->ptError()/track3 ->pt() < 0.1                                    // 4- σ(pT)/pT < 10                                
+     && fabs(track3 ->dz(vxx ->position())/sqrt(pow(track3->dzError(),2)+pow(vxx->zError(),2))) < 3.0         // 5- dz/ σ(dz) < 3
+     && fabs(track3 ->dxy(vxx ->position())/sqrt(pow(track3->dxyError(),2)+pow(vxx->xError(),2)+pow(vxx->yError(),2)) < 3.0)) //6-dxy/σ(dxy)
+{
+//fill histograms after tracks selection
+histod0 ->Fill(track3 ->dxy());
+histodz ->Fill(track3 ->dz());
+histod0pv ->Fill(track3 ->dxy(vxx ->position()));
+histodzpv ->Fill(track3 ->dz(vxx ->position()));
+
 p = p+1;
+}}
 
-}
 
+//************************************************************************************************************************************//
 
 
    
@@ -241,24 +261,25 @@ p = p+1;
    {
        // tracks selection for track
          // 1- high quality
-if (track -> quality(reco::Track::qualityByName("highPurity"))                  // 1- high quality
-     && abs (track -> eta()) < 2.4                                              // 2- |η| < 2.4
-     && track ->pt() >0.1 && track ->pt() <5.0                                  // 3- pt  0.1 GeV/c < pt <5.0 GeV/c.
-     && track ->ptError()/track ->pt() < 0.1                                    // 4- σ(pT)/pT < 10 %
-     && abs(track ->dz())/ track ->dzError() < 3.0                              // 5- dz/ σ(dz) < 3
-     && abs(track ->dxy())/ track ->dxyError() < 3.0    )                       // 6- dxy/ σ(dxy) <3
-       // numer of tracks after selections
-//{j = j+1;
+
+if (track -> quality(reco::Track::qualityByName("highPurity"))                  // 1- high quality     
+     && fabs(track -> eta()) < 2.4                                              // 2- |η| < 2.4
+     && track ->pt() >0.4 &&track ->pt() <5.0                                                        // 3-pt >0.4 GeV/c.
+     && track ->ptError()/track ->pt() < 0.1                                                                    // 4- σ(pT)/pT < 10                                
+     && fabs(track ->dz(vxx ->position())/sqrt(pow(track->dzError(),2)+pow(vxx->zError(),2))) < 3.0                         // 5- dz/ σ(dz) < 3
+     && fabs(track ->dxy(vxx ->position())/sqrt(pow(track->dxyError(),2)+pow(vxx->xError(),2)+pow(vxx->yError(),2)) < 3.0)) //6-dxy/σ(dxy)<3
+
 {
    for (reco::TrackCollection::const_iterator track1 = tracks->begin() ; track1 != tracks->end(); track1 ++ )
    {
       // tracks selection for track1
-if (track1 -> quality(reco::Track::qualityByName("highPurity"))                 // 1- high quality
-     && abs (track1 -> eta()) < 2.4                                             // 2- |η| < 2.4
-     &&track1 ->pt() >0.1 && track1 ->pt() <5.0                                 // 3- pt  0.1 GeV/c < pt <5.0 GeV/c.
-     &&track1 ->ptError()/track1 ->pt() < 0.1                                   // 4- σ(pT)/pT < 10 %
-     &&abs(track1 ->dz())/ track1 ->dzError() < 3.0                             // 5- dz/ σ(dz) < 3
-     &&abs(track1 ->dxy())/ track1 ->dxyError() < 3.0      )                    // 6- dxy/ σ(dxy) <3
+
+if (track1 -> quality(reco::Track::qualityByName("highPurity"))                  // 1- high quality     
+     && fabs(track1 -> eta()) < 2.4                                              // 2- |η| < 2.4
+     &&track1 ->pt() >0.4 &&track1 ->pt() <5.0                                  // 3- pt  0.1 GeV/c < pt <5.0 GeV/c.
+     && track1 ->ptError()/track1 ->pt() < 0.1                                    // 4- σ(pT)/pT < 10                                
+     && fabs(track1 ->dz(vxx ->position())/sqrt(pow(track1->dzError(),2)+pow(vxx->zError(),2))) < 3.0         // 5- dz/ σ(dz) < 3
+     && fabs(track1 ->dxy(vxx ->position())/sqrt(pow(track1->dxyError(),2)+pow(vxx->xError(),2)+pow(vxx->yError(),2)) < 3.0)) //6-dxy/σ(dxy)
 {                                                                           
        if (abs(track ->eta() - track1 ->eta()) <= 0.1)
      { k1 =k1+1;}
@@ -283,14 +304,17 @@ if (track1 -> quality(reco::Track::qualityByName("highPurity"))                 
        k1 =0; y2 =0; m5 =0; }//}
    
    histoNo ->Fill(tracks ->size());
-//cout <<"-----------------------------------------------------------------"<<endl;   
+      
+
+
 cout <<"----------------------------------------------------------------------------------" <<endl;
 cout <<"*** the number of tracks in this event = " << tracks ->size()<<endl;
  //  g ->SetPoint(1,i,tracks ->size());
-histoSl ->Fill(j);
+histoSl1 ->Fill(j);
 cout <<"*** the numer of tracks 1st after selection in this event = " <<j <<endl;
 j=0;
 
+histoSl2 ->Fill(p);
 cout <<"*** the numer of tracks  2nd after selection in this event = " <<p <<endl;
 p=0;
 
@@ -322,7 +346,7 @@ cout <<"********************************** ending of the event *****************
 
 // ------------ method called once each job just before starting event loop  ------------
 void 
-DemoAnalyzer5::beginJob()
+DemoAnalyzer6::beginJob()
 {
 
 
@@ -330,11 +354,20 @@ DemoAnalyzer5::beginJob()
   edm::Service<TFileService> fs;
 
 //histograms
-   histo1 = fs->make<TH1D>("eta(0.1)" , "eta(0.1)" , 100 , 0 , 80 );
-   histo2 = fs->make<TH1D>("eta(0.2)" , "eta(0.2)" , 100 , 0 , 200 );
-   histo5 = fs->make<TH1D>("eta(0.5)" , "eta(0.5)" , 100 , 0 , 300 );   
-   histoNo = fs->make<TH1D>("tracksno." , "tracksno." , 600 , 0 , 15000 );
-   histoSl = fs->make<TH1D>("trackssl." , "tracksSl." , 100 , 0 , 500 );
+   histo1 = fs->make<TH1D>("eta(0.1)" , "eta(0.1)" , 100 , 0 , 300 );
+   histo2 = fs->make<TH1D>("eta(0.2)" , "eta(0.2)" , 100 , 0 , 600 );
+   histo5 = fs->make<TH1D>("eta(0.5)" , "eta(0.5)" , 100 , 0 , 1000 );   
+   histoNo = fs->make<TH1D>("tracksno." , "tracksno." , 200 , 0 , 16000 );
+   histoSl1 = fs->make<TH1D>("trackssl1." , "tracksSl1." , 100 , 0 , 1000 );
+   histoSl2 = fs->make<TH1D>("trackssl2." , "tracksSl2." , 200 , 0 , 4000 );
+
+  histod0 = fs->make<TH1D>("tracksd0" , "tracksd0" , 100 , -3 , 3 );
+  histod0pv = fs->make<TH1D>("tracksd0pv" , "tracksd0pv" , 100 , -3 , 3 );
+  histodz = fs->make<TH1D>("tracksdz" , "tracksdz" , 100 , -10 ,10 );
+  histodzpv = fs->make<TH1D>("tracksdzpv" , "tracksdzpv" , 100 , -10, 10) ;
+
+
+
   //graphno = fs->make<TH1D>("gtracksno." , "gtracksno." , 1000 , 0 , 15000 );
 //f= new TFile ("test.root","RECREATE");
 //g = new TGraph();
@@ -344,7 +377,7 @@ DemoAnalyzer5::beginJob()
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
-DemoAnalyzer5::endJob() 
+DemoAnalyzer6::endJob() 
 {
 //g->Write();
 //f->Close();
@@ -354,7 +387,7 @@ DemoAnalyzer5::endJob()
 // ------------ method called when starting to processes a run  ------------
 /*
 void 
-DemoAnalyzer5::beginRun(edm::Run const&, edm::EventSetup const&)
+DemoAnalyzer6::beginRun(edm::Run const&, edm::EventSetup const&)
 {
 }
 */
@@ -362,7 +395,7 @@ DemoAnalyzer5::beginRun(edm::Run const&, edm::EventSetup const&)
 // ------------ method called when ending the processing of a run  ------------
 /*
 void 
-DemoAnalyzer5::endRun(edm::Run const&, edm::EventSetup const&)
+DemoAnalyzer6::endRun(edm::Run const&, edm::EventSetup const&)
 {
 }
 */
@@ -370,7 +403,7 @@ DemoAnalyzer5::endRun(edm::Run const&, edm::EventSetup const&)
 // ------------ method called when starting to processes a luminosity block  ------------
 /*
 void 
-DemoAnalyzer5::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+DemoAnalyzer6::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
 {
 }
 */
@@ -378,14 +411,14 @@ DemoAnalyzer5::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup
 // ------------ method called when ending the processing of a luminosity block  ------------
 /*
 void 
-DemoAnalyzer5::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+DemoAnalyzer6::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
 {
 }
 */
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
-DemoAnalyzer5::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+DemoAnalyzer6::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   //The following says we do not know what parameters are allowed so do no validation
   // Please change this to state exactly what you do use, even if it is no parameters
   edm::ParameterSetDescription desc;
@@ -394,4 +427,4 @@ DemoAnalyzer5::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
 }
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(DemoAnalyzer5);
+DEFINE_FWK_MODULE(DemoAnalyzer6);
